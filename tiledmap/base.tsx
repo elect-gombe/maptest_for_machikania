0e54a788e4fc3780b90b693aa5640f9cea39f2ea
elect-gombe <13552050+elect-gombe@users.noreply.github.com>
2018-10-11 22:03:26 +0900

    Add sample maps

---
+++ tiledmap/base.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="base" tilewidth="16" tileheight="16" tilecount="2336" columns="8">
+ <image source="../result.bmp" trans="ff00ff" width="128" height="3118"/>
+</tileset>
---
+++ tiledmap/region.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="region" tilewidth="16" tileheight="16" tilecount="2" columns="2">
+ <image source="map chips/1537754102083.gif" width="32" height="16"/>
+</tileset>
